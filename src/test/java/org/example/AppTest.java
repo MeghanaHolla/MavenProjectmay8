ee6c1a09f3d50fa59f698a0425401138f396f29e
Meghana Holla <meghanaholla8526@gmail.com>
2025-05-08 11:49:48 +0530

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    WebDriver driver;
+    @BeforeTest
+    public void a(){
+         driver = new ChromeDriver();
+        driver.get("https://magento.softwaretestingboard.com/customer/account/create/");
+
+    }
+    @Test
+    public void b(){
+        String expectedTitle = "Create New Customer Account";
+        String actualTitle = driver.getTitle();
+        if (expectedTitle.equals(actualTitle)){
+            System.out.println("Test Case passed");
+        }
+        else{
+            System.out.println("Test Case Failed");
+        }
+
+    }
+    @AfterTest
+    public void c(){
+        driver.close();
+    }
+
+}
